bc3a7c7ac90f4cfa2f7349a4bde932c1bf585370
FTMNRETK <101818539+FTMNRETK@users.noreply.github.com>
2023-11-21 21:35:38 +0300

    .

---
+++ index1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<base  target="_blank">
+</head>
+        <body>
+                
+      
+<FORM action="işlem.php" method="get">
+  <select id="liste" name="liste">
+ <option value="1" >02 MART 2023</option>
+ <option value="2">01 MART 2023</option>
+ <option value="03 Mart 2023">03 MART 2023</option>
+ <option value="04 Mart 2023">04 MART 2023</option>
+  <INPUT TYPE="submit" value="Submit">
+
+</select>
+
+</FORM>
+
+
+
+</body>
+</html>
